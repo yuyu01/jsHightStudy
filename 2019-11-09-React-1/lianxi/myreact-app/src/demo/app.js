bd0456636c8b4ab6d25934eb80b6b4cc5cd5d57d
362935347@qq.com <362935347@qq.com>
2019-11-17 21:32:31 +0800

    react

--- 2019-11-09-React-1/lianxi/myreact-app/src/demo/app.js
+++ 2019-11-09-React-1/lianxi/myreact-app/src/demo/app.js
@@ -2,35 +2,42 @@ import React, {Component} from "react"
 import "./index.css"
 import List from './list'
 let datas = {
-    family: {
-        title: '家人',
-        list: [
-            {name: '爸爸'},
-            {name: '妈妈'}
-        ]
-    },
-    friend: {
-        title: '朋友',
-        list: [
-            {name: '张三'},
-            {name: '李四'},
-            {name: '王五'}
-        ]
-    },
-    customer: {
-        title: '客户',
-        list: [
-            {name: '阿里'},
-            {name: '腾讯'},
-            {name: '头条'}
-        ]
-    }
+        family: {
+            title: '家人',
+            list: [
+                {name: '爸爸'},
+                {name: '妈妈'}
+            ]
+        },
+        friend: {
+            title: '朋友',
+            list: [
+                {name: '张三'},
+                {name: '李四'},
+                {name: '王五'}
+            ]
+        },
+        customer: {
+            title: '客户',
+            list: [
+                {name: '阿里'},
+                {name: '腾讯'},
+                {name: '头条'}
+            ]
+        }
 };
 export default class App extends Component{
+    state={
+        show:""
+    }
+    
     render(){
+        const callB = (d)=>{
+            this.setState({show:d})
+        }
         return (<div className = "friend-list">
         {Object.keys(datas).map(item=>{
-            return <List key={item} data = {datas[item]}></List>
+            return <List key={item} data = {datas[item]} show = {this.state.show} callBack = {callB}></List>
         })}
         </div>);
     }
--- 2019-11-09-React-1/lianxi/myreact-app/src/demo/list.js
+++ 2019-11-09-React-1/lianxi/myreact-app/src/demo/list.js
@@ -1,19 +1,15 @@
 import React,{Component} from "react"
 
 export default class List extends Component{
-    state={
-        show:false
-    };
+
     render(){
-        let {show} = this.state;
-        let {data} = this.props;
+        let {data,show,callBack} = this.props;
         let {title,list} = data;
+        console.log(callBack)
         return (
-        <dl className = {"friend-group "+(show?"expanded":"")}>
+        <dl className = {"friend-group "+(show === title?"expanded":"")}>
             <dt onClick = {()=>{
-                this.setState({
-                    show:!show
-                });
+                callBack(title)
             }}>
             {title}</dt>
             {list.map((item,idx)=><dd key={idx}>{item.name}</dd>)}
